c0d50d9d1c62442d34eede5f7efc2ffe8485f1e0
peterschaadt <pete787f@easv365.dk>
2020-03-25 11:15:05 +0100

    CSS GRID Tilføjet

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,46 @@
 <template>
   <div id="app">
-    <div class="container">
-      <Navigation />
-
-    <router-view/>
-    </div>
+<div class="wrapper">
+        <header class="main-head"><h1>Holly' The Dog's Petshop</h1></header>
+        <Navigation />
+        <article class="content">
+            <h2>LATEST PRODUCTS</h2>
+            <p>In this layout, we display the areas in source order for any screen less that 500 pixels wide. We go to a two column layout, and then to a three column layout by redefining the grid, and the placement of items on the grid.</p>
+        <ul class="listing">
+  <li>
+    <h3>Item One</h3>
+    <div class="body"><p>The content of this listing item goes here.</p></div>
+    <div class="cta"><a href="">Call to action!</a></div>
+  </li>
+   <li>
+     <h3>Item Two</h3>
+     <div class="body"><p>The content of this listing item goes here.</p></div>
+     <div class="cta"><a href="">Call to action!</a></div>
+   </li>
+   <li class="wide">
+     <h3>Item Three</h3>
+     <div class="body"><p>The content of this listing item goes here.</p> 
+     <p>This one has more text than the other items.</p>
+     <p>Quite a lot more</p>
+     <p>Perhaps we could do something different with it?</p></div>
+     <div class="cta"><a href="">Call to action!</a></div>
+    </li>
+    <li>
+     <h3>Item Four</h3>
+     <div class="body"><p>The content of this listing item goes here.</p></div>
+     <div class="cta"><a href="">Call to action!</a></div>
+    </li>
+     <li>
+     <h3>Item Five</h3>
+     <div class="body"><p>The content of this listing item goes here.</p></div>
+      <div class="cta"><a href="">Call to action!</a></div>
+    </li>
+</ul>        
+        </article> 
+        <aside class="side"><h2>BASKET</h2></aside>
+        <div class="ad">Advertising</div>
+        <footer class="main-footer">The footer</footer>
+</div>
   </div>
 </template>
 
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,11 +1,12 @@
 <template>
-<nav><ul>
-      <router-link tag="li" to="/">Home</router-link>
-      <router-link tag="li" to="/about">Aboutdw</router-link>
-      <router-link style="float:right" tag="li" to="/about">Login</router-link>
-      
-      </ul>
-</nav>
+        <nav class="main-nav">
+          <h2>CATEGORIES</h2>
+            <ul>
+                <li><a href="">Nav 1</a></li>
+                <li><a href="">Nav 2</a></li>
+                <li><a href="">Nav 3</a></li>
+            </ul>
+        </nav>
 </template>
 
 <script>
@@ -16,31 +17,5 @@
 
 <style scoped>
 
-ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-  background-color: #333;
-}
 
-li {
-  float: left;
-}
-
-li{
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-
-li :hover:not(.active) {
-  background-color: #111;
-}
-
-.active {
-  background-color: #4CAF50;
-}
 </style>
